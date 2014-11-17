2ed1f93e9c96f7d77f8ca867a966c49b6eaf90d7
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-11-17 01:01:08 -0700

    optimized angry children using sorting

--- AngryChildren/main.cpp
+++ AngryChildren/main.cpp
@@ -1,50 +1,28 @@
 #include <cmath>
 #include <cstdio>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int unfairness = -1;
-int N, K, minc, maxc, ufair;
-int *candies, *subset;
-
-void check() {
-	ufair = 0;
-	minc = subset[0];
-	maxc = subset[0];
-
-	for (int i = 0; i < K; i++) {
-		minc = min(minc,subset[i]);
-		maxc = max(maxc, subset[i]);
-	}
-
-	ufair = maxc - minc;
-
-	if (ufair < unfairness || unfairness == -1) {
-		unfairness = ufair;
-	}
-}
-
-void process(int subsetSize, int nextIndex) {
-	if (subsetSize == K) {
-		check();
-	} else {
-		for (int j = nextIndex; j < N; j++) {
-			subset[subsetSize] = candies[j];
-			process(subsetSize + 1, j + 1);
-		}
-	}
-}
-
 int main() {
+	int N, K;
 	cin >> N >> K;
-	candies = new int[N];
-	subset = new int[K];
+	int* candies = new int[N];
 
 	for (int i = 0; i < N; i++)
 		cin >> candies[i];
 
-	process(0, 0);
+	sort(candies, candies + N);
+
+	int unfairness = -1;
+
+	for (int i = 0; i <= N - K; i++) {
+		int ufair = candies[i + K - 1] - candies[i];
+		if (unfairness == -1 || ufair < unfairness) {
+			unfairness = ufair;
+		}
+	}
 
 	cout << unfairness << endl;
 
